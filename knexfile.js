299419dc424e86564f7d2b556c423a9d95af4834
William Luqui <william.luqui@gmail.com>
2022-09-15 01:13:52 -0300

    login de usuario

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config({path: 'path-to-.env'})
 const {HOST,PORT_DATABASE,USER,PASSWORD_DATABASE,DATABASE,
     CLIENT_DATABASE} = process.env
 
@@ -7,11 +7,11 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host : HOST || '127.0.0.1',
-      port : PORT_DATABASE || 8080,
-      user : USER || 'root',
-      password : PASSWORD_DATABASE || "123456",
-      database : DATABASE || 'database_name'
+      host : HOST ,
+      port : PORT_DATABASE ,
+      user : USER ,
+      password : PASSWORD_DATABASE ,
+      database : DATABASE 
   },
   migrations:{
     directory: `${__dirname }/src/database/migrations`,
