c41dd6f0ceadce63438a1fc7b077561589efec87
William Luqui <william.luqui@gmail.com>
2022-09-16 19:08:50 -0300

    cookies

--- src/server.js
+++ src/server.js
@@ -7,8 +7,8 @@ const {COOKIE_PARSE_KEY} = process.env;
 const {SECRET_SESION} = process.env;
 const bodyParser = require('body-parser');
 const router = require('./routes/routes');
-const session = require('express-session')
-const flash = require('express-flash')
+const session = require('cookie-session');
+const flash = require('express-flash');
 const cookieParser = require("cookie-parser");
 
 
